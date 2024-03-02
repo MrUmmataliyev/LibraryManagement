9583adbaa30a98cdc438dd09c6bd9804d4a093fe
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-03-02 13:22:58 +0500

    Started IService

---
+++ LibraryManagement.Application/Abstractions/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagement.Domain.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Application.Abstractions
+{
+    public interface IBookRepository : IBaseRepository<Book>
+    {
+    }
+}
---
+++ LibraryManagement.Infrastructure/BaseRepositories/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagement.Application.Abstractions;
+using LibraryManagement.Domain.Entities.Models;
+using LibraryManagement.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Infrastructure.BaseRepositories
+{
+    public class BookRepository : BaseRepository<Book>, IBookRepository
+    {
+        public BookRepository(LibraryManagementDbContext context) : base(context) 
+        { 
+        
+        }
+    }
+}
